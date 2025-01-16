9930bd0b81a8b48b682b061eb44e95c3d9c7e6c5
Yedige Ashmet <55581804+gradedSystem@users.noreply.github.com>
2025-01-16 19:00:03 +0500

    [fix][s] Small fix to the script/add requirements.txt

--- scripts/process.py
+++ scripts/process.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import requests
-import os
 import csv
 
 SOURCE_URL = 'http://efele.net/maps/fips-10/data/fips-414.txt'
@@ -11,24 +10,15 @@ def retrieve(source_url):
         response = requests.get(source_url)
         file.write(response.content)
 
-
-# The original file formatting is so terrible, e.g.
-# UKY9_414_414_county borough_bwrdeistref sirol___Rhondda Cynon Taff [English]; Rhondda Cynon Taf [Welsh]__
-# RS82_414_414_autonomous okrug_avtonomnyy okrug___Ust'-Ordynskiy Buryatskiy Avtonomnyy Okrug__
-# UKY5_414_414_county borough_bwrdeistref sirol___Neath Port Talbot [English]; Castell-nedd Port Talbot [Welsh]__
-
-# mostly the fields are divided by triple underscore, but sometimes double, but sometimes by only one!
-# so don't be scared of my parse function :)
-
 def parse():
     def clean(text):
         return text.strip('_').replace('_', ' ').replace(',', '')
 
-    destination = open('data/data.csv', 'w')
+    destination = open('data/data.csv', 'w', encoding='utf-8', newline='')
     writer = csv.writer(destination)
     writer.writerow(['region_code', 'region_division', 'region_name'])
 
-    source = open('archive/data.txt')
+    source = open('archive/data.txt', encoding='utf-8')
     for line in source:
         line = line.rstrip().strip('_').replace('414', '')
         try:
